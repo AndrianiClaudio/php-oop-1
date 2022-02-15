4f700c4b4fb10308d9b98960371d6758d89f2a9e
AndrianiClaudio <claudioandriani1994@gmail.com>
2022-02-15 15:08:05 +0100

    Modifica costruttore. primi metodi Setter

--- Movie.php
+++ Movie.php
@@ -9,29 +9,44 @@
  * vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
  */
 
-
 // è definita una classe ‘Movie’
 class Movie {
   // variabili d’istanza
+  // variabili set in costruttore
   public $title;
   public $originalLanguage;
+  public $releaseDate;
+  // variabili da includere in funzione set
   public $overview;
   public $popularity;
   public $cast;
-  public $release_date;
 
   // Costruttore
-  function __construct($_title,$_originalLanguage)
+  
+  function __construct(string $_title,string $_originalLanguage,string $_releaseDate)
   {
     $this->title = $_title;
     $this->originalLanguage = $_originalLanguage;
+    $this->release_date = $_releaseDate;
   }
   // Metodi
+  // Setter
+  public function setOverview(string $overview): void 
+  {
+    $this->overview = $overview;
+  }
+  public function setPopularity(string $popularity): void 
+  {
+    $this->popularity = $popularity;
+  }
+  public function setCast(array $cast): void 
+  {
+    $this->cast = $cast;
+  }
 }
 
 
 // Istanze
-$fightClub = new Movie('Fight Club','en');
-var_dump ($fightClub);
-echo $fightClub->title;
+$fightClub = new Movie('Fight Club','en','1999-10-15');
+$fightClub->setCast(['Brad Pitt','Edward Norton']);
 ?>
