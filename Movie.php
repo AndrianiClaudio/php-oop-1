f043ff327362141ffc663212b30b4f08b13fea12
AndrianiClaudio <claudioandriani1994@gmail.com>
2022-02-15 15:46:23 +0100

    stampa prima istanza film inserita

--- Movie.php
+++ Movie.php
@@ -27,7 +27,7 @@ class Movie {
   {
     $this->title = $_title;
     $this->originalLanguage = $_originalLanguage;
-    $this->release_date = $_releaseDate;
+    $this->releaseDate = $_releaseDate;
   }
   // Metodi
   // Setter
@@ -35,7 +35,7 @@ class Movie {
   {
     $this->overview = $overview;
   }
-  public function setPopularity(string $popularity): void 
+  public function setPopularity(float $popularity): void 
   {
     $this->popularity = $popularity;
   }
@@ -43,10 +43,100 @@ class Movie {
   {
     $this->cast = $cast;
   }
+  // Getter
+  public function getTitle() {
+    return $this->title;
+  }
+  public function getOriginalLanguage() {
+    return $this->originalLanguage;
+  }
+  public function getReleaseDate() {
+    return $this->releaseDate;
+  }
+  public function getOverview() {
+    return $this->overview;
+  }
+  public function getPopularity() {
+    return $this->popularity;
+  }
+  public function getCast() {
+    return $this->cast;
+  }
 }
 
-
+$fightClub__setterData = [
+  'Overview' => 'A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground "fight clubs" forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.',
+  'Popularity' => 58.932,
+  'Cast' => [
+    'Narratore' => 'Edward Norton',
+    'Tyler-Durden' => 'Brad Pitt',
+    'Helena-Bonham-Carter' => 'Marla Singer',
+    'Jared-Leto' => 'Faccia d\'Angelo',
+    'Meat-Loaf' => 'Robert "Bob" Paulson',
+    'Zach-Grenier' => 'Richard Chesler',
+    'David-Andrews' => 'Thomas',
+    'Rachel-Singer' => 'Chloe',
+    'Tim-De-Zarn' => 'ispettore Bird',
+    'Ezra-Buzzington' => 'ispettore Dent',
+    'David-Lee-Smith' => 'Walter',
+    'Eion-Bailey' => 'Ricky',
+    'Evan-Mirand' => 'Steph',
+    'Thom-Gossom-Jr.' => 'detective Stern',
+    'Peter-Iacangelo' => 'Lou',
+    'Joo-n-B-.-Kim' => 'Raymond K. Hessel',
+    'Michael-ShamusWiles' => 'Barista',
+  ],
+];
 // Istanze
+// in costruttore vengono impostati title,originalLanguage e releaseDate
 $fightClub = new Movie('Fight Club','en','1999-10-15');
-$fightClub->setCast(['Brad Pitt','Edward Norton']);
+// usiamo i nostri metodi setter per aggiungere le altre caratteristiche del film fight club
+$fightClub->setOverview($fightClub__setterData['Overview']);
+$fightClub->setPopularity($fightClub__setterData['Popularity']);
+$fightClub->setCast($fightClub__setterData['Cast']);
+// var_dump($fightClub);
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Movie</title>
+  <!-- link file css -->
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <header class="header">
+    <h1>Movie</h1>
+    <hr>
+  </header>
+  <div class="container">
+    <!-- stampa film prima stanza -->
+    <div class="film">
+      <ul>
+        <?php
+        foreach ($fightClub as $attr => $val) {
+          // stampa in caso di dato con array, bigono di altro foreach per stampare tutti gli elementi
+          if(gettype($val) === 'array') { // si potrebbe anche fare se attr === Cast ... ma se aggiungiamo altri array potrebbe tornare piu utile cosi
+            echo '<li><h3 class="bold-item">'. $attr .'</h3><li>';
+            echo '<ul>';
+            foreach ($val as $key => $item) {
+              echo '<li><h4 class="inline-h">'.str_replace('-',' ',$key).'</h4>' . $item . '</li>';
+            }
+            echo '</ul>';
+          } else if($val === null) {
+            echo '<li>'.$attr .'mancante </li>';
+          }
+          else { // caso in cui dato sia stringa o comunque un valore visibile
+            echo '<li><h3>'. $attr. '</h3>'. $val. '</li>';
+          }
+        }
+        ?>
+      </ul>
+    </div>
+  </div>
+</body>
+</html>
