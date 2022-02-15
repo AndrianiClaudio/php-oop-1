ec208463f2b049ac7b86277fe69764ea258d8492
AndrianiClaudio <claudioandriani1994@gmail.com>
2022-02-15 14:27:24 +0100

    Creazione repo. creato Movie.php

---
+++ Movie.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * CONSEGNA
+ * create un file Movie.php in cui:
+ * è definita una classe ‘Movie’
+ * => all’interno della classe sono dichiarate delle variabili d’istanza
+ * => all’interno della classe è definito un costruttore
+ * => all’interno della classe è definito almeno un metodo
+ * vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
+ */
+
+
+// è definita una classe ‘Movie’
+class Movie {
+  public $title;
+  public $overview;
+  public $popularity;
+  public $cast;
+  public $release_date;
+
+  
+}
+
+?>
